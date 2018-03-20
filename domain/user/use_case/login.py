cc02bee88ac5d7aad25e66d3b09eec1c49088396
Víctor Ahuir <vahuir@onestic.com>
2018-03-20 18:02:45 +0100

    Remove infraestructure dependencies from api

--- domain/user/use_case/login.py
+++ domain/user/use_case/login.py
@@ -1,14 +1,15 @@
 from domain.user.entity.user import User as UserEntity
 from domain.user.service.user import User as UserService
+from infraestructure.user.repository.user import User as user_repository
 
 
 class Login:
     user_repository = None
     user_service = None
 
-    def __init__(self, user_repository):
-        self.user_repository = user_repository
-        self.user_service = UserService(user_repository)
+    def __init__(self):
+        self.user_repository = user_repository()
+        self.user_service = UserService(self.user_repository)
 
     def login(self, email, password):
         my_user = UserEntity(email, password)
