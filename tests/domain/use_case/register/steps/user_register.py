d4f80776c2e1c495635926a55885a669864f4374
Víctor Ahuir <vahuir@onestic.com>
2018-03-21 23:09:09 +0100

    changed tests to be more simple

--- tests/domain/use_case/register/steps/user_register.py
+++ tests/domain/use_case/register/steps/user_register.py
@@ -6,41 +6,42 @@ from unittest.mock import MagicMock
 from infraestructure.user.repository.user import User as UserRepository
 
 
-@given('I try to register a user with username empty and password "{password}"')
-@given('I try to register a user with username "{username}" and password empty')
-@given('I try to register a user with username "{username}" and password "{password}"')
+@given('a user with username empty and password "{password}"')
+@given('a user with username "{username}" and password empty')
+@given('a user with username "{username}" and password "{password}"')
+@given('a user with username empty and password empty')
 def new_user_valid(context, username=None, password=None):
     context.email = username
     context.password = password
+    context.user_repository = UserRepository()
+
+
+@when('we try to register the user valid')
+def register_user(context):
+    context.user_repository.save = MagicMock(return_value=UserEntity(context.email, context.password))
+    context.register_use_case = RegisterUseCase()
+    context.register_use_case.user_service.repository = context.user_repository
+    context.user = context.register_use_case.register(email=context.email, password=context.password)
+
+
+@when('we try to register the user invalid')
+def register_user(context):
     try:
-        context.my_new_user = UserEntity(username, password)
-        context.exc = None
+        context.user_repository.save = MagicMock(return_value=UserEntity(context.email, context.password))
+        context.register_use_case = RegisterUseCase()
+        context.register_use_case.user_service.repository = context.user_repository
+        context.user = context.register_use_case.register(email=context.email, password=context.password)
     except Exception as ex:
         context.exc = str(ex)
         context.my_new_user = None
 
 
-@when('we try to register the user')
-def register_user(context):
-    # Mocking the repository
-    context.user_repository = UserRepository()
-    context.user_repository.save = MagicMock(return_value=context.my_new_user)
-    register_use_case = RegisterUseCase()
-    register_use_case.user_repository = context.user_repository
-    register_use_case.user_service.repository = context.user_repository
-    try:
-        context.user = register_use_case.register(email=context.email, password=context.password)
-    except Exception as ex:
-        context.exc = str(ex)
-        context.my_new_user = register_use_case.my_user
-
-
 @then('the user is saved')
 def check_new_user_saved(context):
-    assert context.user == context.my_new_user
+    assert context.user.email == context.email
+    assert context.user.password == context.password
 
 
 @then('the error "{error}" is shown')
 def check_new_user_without_any_field(context, error):
     assert context.exc == error
-
