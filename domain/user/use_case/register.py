e3b1c2e43a9304dfa5edf597a08588f171a985c0
Víctor Ahuir <vahuir@onestic.com>
2018-03-21 22:47:48 +0100

    Try to test the use_case

--- domain/user/use_case/register.py
+++ domain/user/use_case/register.py
@@ -6,12 +6,14 @@ from infraestructure.user.repository.user import User as UserRepository
 class Register:
     user_service = None
     user_repository = None
+    my_user = None
 
     def __init__(self):
         self.user_repository = UserRepository()
         self.user_service = UserService(self.user_repository)
 
     def register(self, email, password):
-        my_user = UserEntity(email, password)
-        user = self.user_service.register(my_user)
+        self.my_user = UserEntity(email, password)
+
+        user = self.user_service.register(self.my_user)
         return user
